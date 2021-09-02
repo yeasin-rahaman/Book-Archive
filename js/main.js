22887855a1178912fc1ef01edbbcbd3b3c186a29
Dev70 <Shanto9585>
2021-09-02 19:41:02 +0600

    update

--- js/main.js
+++ js/main.js
@@ -42,6 +42,9 @@ const searchBook = () => {
 const showData = (book) => {
   //Total Book Found
   totalBookFound.innerText = ` Total ${book.numFound} Books found`;
+  if (book.numFound == 0) {
+    console.log("something")
+  }
 
 
   // clear the container field
